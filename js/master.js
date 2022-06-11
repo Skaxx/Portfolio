1670a8ea0c40e9f7b650e52b2eae420526e9e028
Patryk Pietrzyk <skaxx69@MacBook-Pro-Patryk.local>
2022-06-11 16:35:51 +0200

    Added JS code and updated footer and navigation

--- js/master.js
+++ js/master.js
@@ -1,3 +1,31 @@
 $(window).on("load", function () {
-    $(".loader-wrapper").fadeOut("slow");
+  $(".loader-wrapper").fadeOut("slow");
 });
+
+function date() {
+  const day = document.querySelector('.day')
+    const month = document.querySelector('.month')
+    const year = document.querySelector('.year')
+    const hours = document.querySelector('.hours')
+    const minutes = document.querySelector('.minutes')
+    const seconds = document.querySelector('.seconds')
+
+    const days = [
+      'Niedziela',
+      'Poniedziałek',
+      'Wtorek',
+      'Środa',
+      'Czwartek',
+      'Piątek',
+      'Sobota'
+    ]
+
+    const runClock = () => {
+      const now = new Date()
+      day.innerText = days[now.getDay()]
+      hours.innerText = now.getHours()
+      minutes.innerText = now.getMinutes()
+    }
+    runClock()
+    setInterval(runClock, 1000)
+}
