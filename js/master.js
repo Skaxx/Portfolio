68fa85df86829faab972c61270c7edd4c7ca91a0
Patryk Pietrzyk <skaxx69@macbook-pro-patryk.home>
2022-09-11 15:46:30 +0200

    Creating English version of this website and adding ENG CV.
    '#' will be ignored, and an empty message aborts the commit. # #
    Committer: Patryk Pietrzyk <skaxx69@macbook-pro-patryk.home> # # On branch
    main # Your branch is up to date with 'origin/main'. # # Changes to be
    committed: # modified:  .DS_Store # modified:  about.html # new file:
    aboutENG.html # modified:  contact.html # new file:  contactENG.html #
    modified:  css/style.css # modified:  css/style.css.map # new file:
    documents/Patryk Pietrzyk - resume.pdf # new file:  img/resume.png #
    modified:  index.html # new file:  indexENG.html # modified:
    js/master.js # new file:  project-c-ENG.html # modified:  project-c.html #
    new file:  project-p-ENG.html # modified:  project-p.html # modified:
    scss/style.scss #

--- js/master.js
+++ js/master.js
@@ -3,29 +3,76 @@ $(window).on("load", function () {
 });
 
 function date() {
-  const day = document.querySelector('.day')
-    const month = document.querySelector('.month')
-    const year = document.querySelector('.year')
-    const hours = document.querySelector('.hours')
-    const minutes = document.querySelector('.minutes')
-    const seconds = document.querySelector('.seconds')
+  const day = document.querySelector(".day");
+  const month = document.querySelector(".month");
+  const year = document.querySelector(".year");
+  const hours = document.querySelector(".hours");
+  const minutes = document.querySelector(".minutes");
+  const seconds = document.querySelector(".seconds");
 
-    const days = [
-      'Niedziela',
-      'Poniedziałek',
-      'Wtorek',
-      'Środa',
-      'Czwartek',
-      'Piątek',
-      'Sobota'
-    ]
+  const days = [
+    "Niedziela",
+    "Poniedziałek",
+    "Wtorek",
+    "Środa",
+    "Czwartek",
+    "Piątek",
+    "Sobota",
+  ];
 
-    const runClock = () => {
-      const now = new Date()
-      day.innerText = days[now.getDay()]
-      hours.innerText = now.getHours()<10?'0'+now.getHours():now.getHours();
-      minutes.innerText = now.getMinutes()<10?'0'+now.getMinutes():now.getMinutes();
+  const runClock = () => {
+    const now = new Date();
+    day.innerText = days[now.getDay()];
+    hours.innerText =
+      now.getHours() < 10 ? "0" + now.getHours() : now.getHours();
+    minutes.innerText =
+      now.getMinutes() < 10 ? "0" + now.getMinutes() : now.getMinutes();
+  };
+
+  runClock();
+  setInterval(runClock, 1000);
+}
+
+function dateENG() {
+  const day = document.querySelector(".day");
+  const month = document.querySelector(".month");
+  const year = document.querySelector(".year");
+  const hours = document.querySelector(".hours");
+  const minutes = document.querySelector(".minutes");
+  const seconds = document.querySelector(".seconds");
+
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  const runClock = () => {
+    const now = new Date();
+    day.innerText = days[now.getDay()];
+    hours.innerText = now.getHours < 10 ? "0" + now.getHours() : now.getHours();
+    minutes.innerText =
+      now.getMinutes() < 10 ? "0" + now.getMinutes() : now.getMinutes();
+  };
+  runClock();
+  setInterval(runClock, 1000);
+}
+
+let PL = document.querySelector(".PL");
+let ENG = document.querySelector(".ENG");
+
+function lngPick() {
+  document.getElementById("lngPick").onchange = function lngPick() {
+    if (this.value == "Polish") {
+      window.location.href = "https://patryk-pietrzyk.pl/index.html";
     }
-    runClock()
-    setInterval(runClock, 1000)
+
+    if (this.value == "English") {
+      window.location.href = "https://patryk-pietrzyk.pl/indexENG.html";
+    }
+  };
 }
